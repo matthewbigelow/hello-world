ecaebffb1ea4949a5308d38bee270aaf23db4de6
Matt Bigelow <bigmatt1200@gmail.com>
2024-03-15 11:04:13 -0400

    daily practice

--- app.py
+++
@@ -1,8 +0,0 @@
-year = int(input("what year"))
-
-age = 2023 - year
-
-if age>=18: 
-    print ("you are allowed")
-
-print ("thank you!")
--- app3.py
+++
@@ -1,3 +0,0 @@
-user = int(input("Please type in a number:"))
-
-print(f"{user} times 5 is {user*5}")
